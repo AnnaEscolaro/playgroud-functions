8cca7386d0c09487008e2d84c618e7ad5ff04668
Anna Escolaro <annaescolaro@hotmail.com>
2023-04-05 17:40:29 -0300

    corrige 5

--- src/challenges.js
+++ src/challenges.js
@@ -36,11 +36,12 @@ function footballPoints(wins, ties) {
 function highestCount(num) {
   let counter = 0;
   let higherNum = num[0];
+  let numToAnalyze = num[0];
   for (let index = 0; index < num.length; index += 1) {
-    if (num[index] >= num[0]) {
+    if (num[index] > higherNum) {
       higherNum = num[index];
     }
-  }
+  };
   for (let index = 0; index < num.length; index += 1) {
     if (higherNum === num[index]) {
       counter += 1;
